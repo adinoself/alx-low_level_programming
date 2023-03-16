b46716bdb49c6267a1133cb6b503943cafe67069
“Joseph <modithejoe@gmail.com>
2023-03-16 12:11:38 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this prints a statement using printf
+ *
+ * Return: should return 0 as required
+ */
+
+int main (void)
+{
+	printf ("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
