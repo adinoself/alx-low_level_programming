061bbf27b58d101046e153261b3c497e00189780
“Joseph <modithejoe@gmail.com>
2023-04-11 14:27:44 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,5 +1,47 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * alloc_grid - Function that returns a pointer to a 2
+ * dimensional array of integers
+ * @width: Parameter width
+ * @height: Parameter height
+ * Return: Pinter but if width or height is 0 or negative, return NULL
+ */
 
+int **alloc_grid(int width, int height)
+{
+	int **xyz;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	xyz = malloc(sizeof(int *) * height);
+	if (xyz == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		xyz[i] = malloc(sizeof(int) * width);
+		if (xyz[i] == NULL)
+		{
+			for (; i >= 0; i--)
+			{
+				free(xyz[i]);
+			}
+			free(xyz);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			xyz[i][j] = 0;
+		}
+	}
+	return (xyz);
+}
