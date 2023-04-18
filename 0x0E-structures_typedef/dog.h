584902a657d33853c1cefa59eed2a700677bd94a
“Joseph <modithejoe@gmail.com>
2023-04-18 04:47:44 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,14 +17,14 @@ struct dog
 };
 
 /**
- * dog_i - typedef for struct dog
+ * dog_t - typedef for struct dog
  */
-typedef struct dog dog_i;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_i *new_dog(char *name, float age, char *owner);
-void free_dog(dog_i *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
